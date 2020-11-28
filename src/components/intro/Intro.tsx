21b3236205c624a61a43c2ad21219272cda1c5cc
roniel06 <roniel06.rc@gmail.com>
2020-11-28 10:46:30 -0400

    proyectInitialization

---
+++ src/components/intro/Intro.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export default class Intro extends React.Component{
+    public render(){
+        return( <p>Edit <code>src/App.tsx</code> and save to reload.</p>)
+    }
+}
